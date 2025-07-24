45c3bcf72e553e3fc20786dc19b3b40da6a0ac9c
new.Lich <lichston1@gmail.com>
2025-07-23 23:04:04 -0300

    Adicionando a página inicial, com a primeira api

--- webpython/urls.py
+++ webpython/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('home', views.home, name= 'home'),
     path('admin/', admin.site.urls),
 ]
 
