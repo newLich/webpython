6812c40196676e0ef486f1e43fb9d7126078f8bb
new.Lich <lichston1@gmail.com>
2025-07-24 23:42:07 -0300

    alterações

--- webpython/urls.py
+++ webpython/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('home', views.home, name= 'home'),
     path('admin/', admin.site.urls),
+    path('get_weather_from_ip/', views.get_weather_from_ip, name="get_weather_from_ip"),
 ]
 
 
--- webpython/views.py
+++ webpython/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
 
 # Create your views here.
 def home(request):
   return render(request, 'index.html')
+
+def get_location_from_ip(ip_address):
+    response = requests.get("http://ip-api.com/json/{}".format(ip_address))
+    return response.json()
+
+def get_weather_from_ip(request):
+  ip_address = request.GET.get("ip")
+  location = get_location_from_ip(ip_address)
+  city = location.get("city")
+  country_code = location.get("countryCode")
+  s = "You're in {}, {}".format(city, country_code)
+  data = {"weather_data": s}
+  return JsonResponse(data)
